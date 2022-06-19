3c198bf284a48c0c44839fc25b13c588b73a6fc9
Emad0007 <BASEMRF007@GMAIL.COM>
2022-06-19 22:29:55 +0100

    commit2

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<meta name="discription" content="PortfolioEmad">
 	<meta name="author" content="Emad MUSTAFA">
 	<meta name="keywords" content="HTML,CSS,JavaScript,PHP">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="icon" href="images/logoportfolio.png" type="image/-icon" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous" />
